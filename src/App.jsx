3958420baa3d33f6f18d6e6a3e25e1cd56c83ec2
DefyCab <love.bellinder@gmail.com>
2021-11-22 21:41:03 +0100

    two first test and basic implementation finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [employees, setEmployees] = useState([]);
+
+  const fetchEmployees = async () => {
+    const response = await axios.get("https://reqres.in/api/users");
+    setEmployees(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const employeeList = employees.map((employee) => {
     return (
-        <>
-        <h1>Employee list</h1>
-            
-        </>
-    )
-}
+      <li key={employee.id}>
+        {employee.first_Name}
+        {`${employee.first_name} ${employee.last_name}`}
+      </li>
+    );
+  });
 
-export default App
+  return (
+    <>
+      <h1>Employee List</h1>
+      <ul data-cy="employee-list">{employeeList}</ul>
+    </>
+  );
+};
 
+export default App;
